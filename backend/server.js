9a90a7184f5ddf69ba336faceb11ce08fbd4a995
Pankaj <pankaj2007acharjee@gmail.com>
2022-03-10 22:01:01 +0530

    publish

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,6 @@ const port = process.env.PORT || 5001;
 
 
 app.listen(port, () => {
-  console.log(`Server at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
